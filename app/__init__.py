ab6f4f678a4234304cb8ab6e449e116bcec70969
Matt Jacobs <capn@capndesign.com>
2016-02-02 16:10:46 -0500

    Adding an instance config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
 from app import views, models
---
+++ instance/config.py.example.py
@@ -0,0 +1,5 @@
+
+# Make a copy of this before launching!
+
+DEBUG = True
+SECRET_KEY = 'you-should-replace-this'
